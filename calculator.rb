1cadd629abae864dfe757a8b127cde10a0b7ec4d
Joshua Rodriguez <josh030596@gmail.com>
2019-08-30 04:13:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 sum = first_number + second_number
  expect(difference).to eq(first_number-second_number)
  expect(product).to eq(first_number*second_number)
- 
+ expect(quotient).to eq(first_number/second_number)
