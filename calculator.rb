d3d53e55e3ca35ca64a29b4e418e2cd897bf8628
Joshua Rodriguez <josh030596@gmail.com>
2019-08-30 04:20:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
-sum = first_number + second_number
 first_number = 5
 second_number = 10 
 difference = first_number-second_number 
 product = first_number*second_number 
 quotient = first_number/second_number
+sum = first_number + second_number 
